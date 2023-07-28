3c922521546a5ad648b2fddeb3e3f9e49fc60edf
엄희성 <ehs0525@naver.com>
2023-07-28 22:23:44 +0900

    swiper 모듈을 이용한 터치 슬라이드 구현하기

--- src/components/Row.js
+++ src/components/Row.js
@@ -1,4 +1,11 @@
 import React, { useCallback, useEffect, useState } from "react";
+import { Navigation, Pagination, Scrollbar, A11y } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
 
 import "./Row.css";
 import axios from "../api/axios";
@@ -37,33 +44,61 @@ const Row = ({ category, id, requestURL, large }) => {
     <section className="row">
       <h2>{category}</h2>
 
-      <div className="slider">
-        <div className="slider__arrow-left">
+      {/* <div className="slider"> */}
+      {/* <div className="slider__arrow-left">
           <span className="slider__arrow" onClick={handleScrollLeft}>
             {"<"}
           </span>
-        </div>
+        </div> */}
 
+      <Swiper
+        // install Swiper modules
+        modules={[Navigation, Pagination, Scrollbar, A11y]}
+        navigation
+        pagination={{ clickable: true }}
+        loop={true}
+        breakpoints={{
+          1378: {
+            slidesPerView: 6,
+            slidesPerGroup: 6,
+          },
+          998: {
+            slidesPerView: 5,
+            slidesPerGroup: 5,
+          },
+          625: {
+            slidesPerView: 4,
+            slidesPerGroup: 4,
+          },
+          0: {
+            slidesPerView: 3,
+            slidesPerGroup: 3,
+          },
+        }}
+      >
         <div id={id} className="posters">
           {contents.map((content) => (
-            <img
-              key={content.id}
-              className={`poster ${large && "poster-large"}`}
-              src={`https://image.tmdb.org/t/p/original/${
-                large ? content.poster_path : content.backdrop_path
-              }`}
-              alt={content.name}
-              onClick={handleClickContent(content)}
-            />
+            <SwiperSlide>
+              <img
+                key={content.id}
+                style={{ padding: "25px 0" }}
+                className={`poster ${large && "poster-large"}`}
+                src={`https://image.tmdb.org/t/p/original/${
+                  large ? content.poster_path : content.backdrop_path
+                }`}
+                alt={content.name}
+                onClick={handleClickContent(content)}
+              />
+            </SwiperSlide>
           ))}
         </div>
-
-        <div className="slider__arrow-right">
+      </Swiper>
+      {/* <div className="slider__arrow-right">
           <span className="slider__arrow" onClick={handleScrollRight}>
             {">"}
           </span>
-        </div>
-      </div>
+        </div> */}
+      {/* </div> */}
 
       {isModalOpen && (
         <ContentsModal {...selected} setIsModalOpen={setIsModalOpen} />
